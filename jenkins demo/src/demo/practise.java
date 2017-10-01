409d811c750a73de3efb00ec8f967b22acb7b650
NitinWason <NitinWason@Nitin>
2017-10-01 00:06:15 -0400

    push to master as well

--- jenkins demo/src/demo/practise.java
+++ jenkins demo/src/demo/practise.java
@@ -12,7 +12,7 @@ public class practise {
 	{
 		
 		System.out.println("hello from  new jenkins");
-		System.out.println("hello from  nitika branch");
+		System.out.println("hello from  nitika branch master");
 	}
 
 }
